bf1a2adb83eb8cd1c706fdf4b735c569588cf658
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-01 11:58:19 -0800

    Create accel-basic01.ino

---
+++ ino-examples/accel-basic01.ino
@@ -0,0 +1,55 @@
+/*
+  Arduino LSM9DS1 - Simple Accelerometer
+
+  This example reads the acceleration values from the LSM9DS1
+  sensor and continuously prints them to the Serial Monitor
+  or Serial Plotter.
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense
+
+  created 10 Jul 2019
+  by Riccardo Rizzo
+
+  This example code is in the public domain.
+*/
+
+#include <Arduino_LSM9DS1.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in G's");
+  Serial.println("X\tY\tZ");
+}
+
+void loop() {
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+    x *= 9.8;
+    y *= 9.8;
+    z *= 9.8;
+
+    Serial.print(IMU.accelerationSampleRate());
+    Serial.print(" Hz  \tX: ");
+    Serial.print(x);
+    Serial.print(" \tY: ");
+    Serial.print(y);
+    Serial.print(" \tZ: ");
+    Serial.println(z);
+  }
+  delay(300);
+}
