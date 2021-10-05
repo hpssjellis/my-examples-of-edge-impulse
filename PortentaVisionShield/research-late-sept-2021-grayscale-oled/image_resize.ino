2a37d9dd8b8918ad6f9d067d28a4493d0d3e3e11
Jeremy Ellis <keyfreemusic@gmail.com>
2021-10-04 18:43:14 -0700

    Create image_resize.ino

---
+++ PortentaVisionShield/research-late-sept-2021-grayscale-oled/image_resize.ino
@@ -0,0 +1,57 @@
+void image_resize_linear(uint8_t *dst_image, uint8_t *src_image, int dst_w, int dst_h, int dst_c, int src_w, int src_h)
+{ /*{{{*/
+    float scale_x = (float)src_w / dst_w;
+    float scale_y = (float)src_h / dst_h;
+
+    int dst_stride = dst_c * dst_w;
+    int src_stride = dst_c * src_w;
+
+    if (fabs(scale_x - 2) <= 1e-6 && fabs(scale_y - 2) <= 1e-6)
+    {
+        image_zoom_in_twice(
+            dst_image,
+            dst_w,
+            dst_h,
+            dst_c,
+            src_image,
+            src_w,
+            dst_c);
+    }
+    else
+    {
+        for (int y = 0; y < dst_h; y++)
+        {
+            float fy[2];
+            fy[0] = (float)((y + 0.5) * scale_y - 0.5); // y
+            int src_y = (int)fy[0];                     // y1
+            fy[0] -= src_y;                             // y - y1
+            fy[1] = 1 - fy[0];                          // y2 - y
+            src_y = DL_IMAGE_MAX(0, src_y);
+            src_y = DL_IMAGE_MIN(src_y, src_h - 2);
+
+            for (int x = 0; x < dst_w; x++)
+            {
+                float fx[2];
+                fx[0] = (float)((x + 0.5) * scale_x - 0.5); // x
+                int src_x = (int)fx[0];                     // x1
+                fx[0] -= src_x;                             // x - x1
+                if (src_x < 0)
+                {
+                    fx[0] = 0;
+                    src_x = 0;
+                }
+                if (src_x > src_w - 2)
+                {
+                    fx[0] = 0;
+                    src_x = src_w - 2;
+                }
+                fx[1] = 1 - fx[0]; // x2 - x
+
+                for (int c = 0; c < dst_c; c++)
+                {
+                    dst_image[y * dst_stride + x * dst_c + c] = round(src_image[src_y * src_stride + src_x * dst_c + c] * fx[1] * fy[1] + src_image[src_y * src_stride + (src_x + 1) * dst_c + c] * fx[0] * fy[1] + src_image[(src_y + 1) * src_stride + src_x * dst_c + c] * fx[1] * fy[0] + src_image[(src_y + 1) * src_stride + (src_x + 1) * dst_c + c] * fx[0] * fy[0]);
+                }
+            }
+        }
+    }
+} /*}}}*/
